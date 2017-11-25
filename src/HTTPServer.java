c7162ed898db8ed709d2e6f953706813402d7902
collin <collinalpert@gmail.com>
2017-11-25 13:46:32 +0100

    #9: Added HTTPServer

---
+++ src/HTTPServer.java
@@ -0,0 +1,52 @@
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HTTPServer {
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(1337), 0);
+            server.createContext("/", new Handler());
+            server.createContext("/login", new LoginHandler());
+            System.out.println("Server wird gestartet...");
+            server.setExecutor(null);
+            server.start();
+            System.out.println("Server ist betriebsbereit");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Handler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            Headers headers = exchange.getRequestHeaders();
+            write("Hallo Welt!", exchange);
+        }
+    }
+
+    private static class LoginHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            write("Hallo Welt! Sie sind eingeloggt", exchange);
+        }
+    }
+
+    private static void write(String text, HttpExchange e) throws IOException {
+        e.getResponseHeaders().add("Content-Type", "text/json; charset=utf-8");
+        e.sendResponseHeaders(200, 0);
+        OutputStream os = e.getResponseBody();
+        os.write(text.getBytes("UTF-8"));
+        os.close();
+    }
+
+
+}
