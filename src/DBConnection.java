e176ba13a4da27fc5d5919c356e1460eb9a2dbd5
ADeviloper <teufel.timo6@gmail.com>
2017-11-25 19:40:53 +0100

    getSubjects returns a set

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,7 +1,10 @@
+import org.json.simple.JSONObject;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
+import java.util.HashMap;
 
 public class DBConnection {
 
@@ -12,14 +15,28 @@ public class DBConnection {
     }
 
     private DBConnection() {
+
         try {
+            final String infoFile = "res/password.txt";
             String[] info = new String(Files.readAllBytes(Paths.get(infoFile))).split(";");
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://" + info[0] + ":3306/notenverwaltung", info[1], info[2]);
-            PreparedStatement statement = connection.prepareStatement("SELECT `text` FROM `test`");
-            ResultSet set = statement.executeQuery();
-            printResult(set);
-        } catch (Exception e) {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://10.23.41.229:3306/notenverwaltung", "notenadmin", info[2]);
+
+            String schuelerId = "1";
+
+            PreparedStatement statement = connection.prepareStatement("SELECT subjects.name FROM users\n" +
+                    "LEFT JOIN relationshipsStudents ON users.id= relationshipsStudents.studentID\n" +
+                    "LEFT JOIN relationshipsTeacher ON relationshipsStudents.classID = relationshipsTeacher.klassenId\n" +
+                    "LEFT JOIN subjects ON relationshipsTeacher.fachId = subjects.id\n" +
+                    "WHERE relationshipsStudents.studentID = ?");
+            statement.setString(1, schuelerId);
+            ResultSet resultSet = statement.executeQuery();
+
+
+
+            printResult(resultSet);
+        }
+        catch (Exception e){
             e.printStackTrace();
         }
 
@@ -27,7 +44,7 @@ public class DBConnection {
 
     private void printResult(ResultSet set) throws SQLException {
         while (set.next()) {
-            String text = set.getString("text");
+            String text = set.getString("name");
             System.out.println(text);
         }
     }
